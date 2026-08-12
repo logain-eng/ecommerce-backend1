ac69b33446316dfe9550861e80c54d86af0be369
logain <logain0561@gmail.com>
2026-08-12 18:32:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);                  
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(process.env.PORT || 5000, () => console.log("Server running on port 5000"));
